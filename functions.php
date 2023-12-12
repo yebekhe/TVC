dbe035d0a52d772a769d7691c44c289a704128b6
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-12-12 23:47:50 +0330

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,309 @@
+<?php
+
+function is_ip($string)
+{
+    $ip_pattern = '/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/';
+    if (preg_match($ip_pattern, $string)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function ip_info($ip)
+{
+    if (is_ip($ip) === false) {
+        $ip_address_array = dns_get_record($ip, DNS_A);
+        if (empty($ip_address_array)) {
+            return null;
+        }
+        $randomKey = array_rand($ip_address_array);
+        $ip = $ip_address_array[$randomKey]["ip"];
+    }
+
+    // List of API endpoints
+    $endpoints = [
+        "https://ipapi.co/{ip}/json/",
+        "https://ipwhois.app/json/{ip}",
+        "http://www.geoplugin.net/json.gp?ip={ip}",
+        "https://api.ipbase.com/v1/json/{ip}",
+    ];
+
+    // Initialize an empty result object
+    $result = (object) [
+        "country" => "XX",
+    ];
+
+    // Loop through each endpoint
+    foreach ($endpoints as $endpoint) {
+        // Construct the full URL
+        $url = str_replace("{ip}", $ip, $endpoint);
+
+        $options = [
+            "http" => [
+                "header" =>
+                    "User-Agent: Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.102011-10-16 20:23:10\r\n", // i.e. An iPad
+            ],
+        ];
+
+        $context = stream_context_create($options);
+        $response = file_get_contents($url, false, $context);
+
+        if ($response !== false) {
+            $data = json_decode($response);
+
+            // Extract relevant information and update the result object
+            if ($endpoint == $endpoints[0]) {
+                // Data from ipapi.co
+                $result->country = $data->country_code ?? "XX";
+            } elseif ($endpoint == $endpoints[1]) {
+                // Data from ipwhois.app
+                $result->country = $data->country_code ?? "XX";
+            } elseif ($endpoint == $endpoints[2]) {
+                // Data from geoplugin.net
+                $result->country = $data->geoplugin_countryCode ?? "XX";
+            } elseif ($endpoint == $endpoints[3]) {
+                // Data from ipbase.com
+                $result->country = $data->country_code ?? "XX";
+            }
+            // Break out of the loop since we found a successful endpoint
+            break;
+        }
+    }
+
+    return $result;
+}
+
+function is_valid($input)
+{
+    if (stripos($input, "…") !== false or stripos($input, "...") !== false) {
+        return false;
+    }
+    return true;
+}
+
+function getFlags($country_code)
+{
+    $flag = mb_convert_encoding(
+        "&#" . (127397 + ord($country_code[0])) . ";",
+        "UTF-8",
+        "HTML-ENTITIES"
+    );
+    $flag .= mb_convert_encoding(
+        "&#" . (127397 + ord($country_code[1])) . ";",
+        "UTF-8",
+        "HTML-ENTITIES"
+    );
+    return $flag;
+}
+
+function detect_type($input)
+{
+    if (substr($input, 0, 8) === "vmess://") {
+        return "vmess";
+    } elseif (substr($input, 0, 8) === "vless://") {
+        return "vless";
+    } elseif (substr($input, 0, 9) === "trojan://") {
+        return "trojan";
+    } elseif (substr($input, 0, 5) === "ss://") {
+        return "ss";
+    } elseif (substr($input, 0, 7) === "tuic://") {
+        return "tuic";
+    } elseif (
+        substr($input, 0, 6) === "hy2://" ||
+        substr($input, 0, 12) === "hysteria2://"
+    ) {
+        return "hy2";
+    } elseif (substr($input, 0, 11) === "hysteria://") {
+        return "hysteria";
+    }
+}
+
+function extractLinksByType($inputString, $configType)
+{
+    $pattern = "/(" . $configType . '):\/\/[^"\'\s]+/';
+    preg_match_all($pattern, $inputString, $matches);
+
+    if (empty($matches[0])) {
+        return null;
+    } else {
+        return $matches[0];
+    }
+}
+
+function configParse($input)
+{
+    $configType = detect_type($input);
+    if ($configType === "vmess") {
+        $vmess_data = substr($input, 8);
+        $decoded_data = json_decode(base64_decode($vmess_data), true);
+        return $decoded_data;
+    } elseif (
+        $configType === "vless" ||
+        $configType === "trojan" ||
+        $configType === "tuic" ||
+        $configType === "hy2"
+    ) {
+        $parsedUrl = parse_url($input);
+        $params = [];
+        if (isset($parsedUrl["query"])) {
+            parse_str($parsedUrl["query"], $params);
+        }
+        $output = [
+            "protocol" => $configType,
+            "username" => isset($parsedUrl["user"]) ? $parsedUrl["user"] : "",
+            "hostname" => isset($parsedUrl["host"]) ? $parsedUrl["host"] : "",
+            "port" => isset($parsedUrl["port"]) ? $parsedUrl["port"] : "",
+            "params" => $params,
+            "hash" => isset($parsedUrl["fragment"])
+                ? $parsedUrl["fragment"]
+                : "",
+        ];
+
+        if ($configType === "tuic") {
+            $output["pass"] = isset($parsedUrl["pass"])
+                ? $parsedUrl["pass"]
+                : "";
+        }
+        return $output;
+    } elseif ($configType === "ss") {
+        $url = parse_url($input);
+        list($encryption_method, $password) = explode(
+            ":",
+            base64_decode($url["user"])
+        );
+        $server_address = $url["host"];
+        $server_port = $url["port"];
+        $name = isset($url["fragment"]) ? urldecode($url["fragment"]) : null;
+        $server = [
+            "encryption_method" => $encryption_method,
+            "password" => $password,
+            "server_address" => $server_address,
+            "server_port" => $server_port,
+            "name" => $name,
+        ];
+        return $server;
+    }
+}
+
+function reparseConfig($configArray, $configType)
+{
+    if ($configType === "vmess") {
+        $encoded_data = base64_encode(json_encode($configArray));
+        $vmess_config = "vmess://" . $encoded_data;
+        return $vmess_config;
+    } elseif (
+        $configType === "vless" ||
+        $configType === "trojan" ||
+        $configType === "tuic" ||
+        $configType === "hy2"
+    ) {
+        $url = $configType . "://";
+        $url .= addUsernameAndPassword($configArray);
+        $url .= $configArray["hostname"];
+        $url .= addPort($configArray);
+        $url .= addParams($configArray);
+        $url .= addHash($configArray);
+        return $url;
+    } elseif ($configType === "ss") {
+        $user = base64_encode(
+            $configArray["encryption_method"] . ":" . $configArray["password"]
+        );
+        $url = "ss://$user@{$configArray["server_address"]}:{$configArray["server_port"]}";
+        if (!empty($configArray["name"])) {
+            $url .= "#" . urlencode($configArray["name"]);
+        }
+        return $url;
+    }
+}
+
+function addUsernameAndPassword($obj)
+{
+    $url = "";
+    if ($obj["username"] !== "") {
+        $url .= $obj["username"];
+        if (isset($obj["pass"]) && $obj["pass"] !== "") {
+            $url .= ":" . $obj["pass"];
+        }
+        $url .= "@";
+    }
+    return $url;
+}
+
+function addPort($obj)
+{
+    $url = "";
+    if (isset($obj["port"]) && $obj["port"] !== "") {
+        $url .= ":" . $obj["port"];
+    }
+    return $url;
+}
+
+function addParams($obj)
+{
+    $url = "";
+    if (!empty($obj["params"])) {
+        $url .= "?" . http_build_query($obj["params"]);
+    }
+    return $url;
+}
+
+function addHash($obj)
+{
+    $url = "";
+    if (isset($obj["hash"]) && $obj["hash"] !== "") {
+        $url .= "#" . $obj["hash"];
+    }
+    return $url;
+}
+
+function is_reality($input)
+{
+    $type = detect_type($input);
+    if (stripos($input, "reality") !== false && $type === "vless") {
+        return true;
+    }
+    return false;
+}
+
+function toSingbox(
+    $configs
+) {
+    $url = "https://api.yebekhe.link/convertor/singbox/index.php";
+
+    $data = [
+        "config" => $configs,
+        "limit" => 0,
+        "tun" => true
+    ];
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    return $result;
+}
+
+function toClash(
+    $configs,
+    $type
+) {
+    $url = "https://api.yebekhe.link/convertor/clash/index.php";
+
+    $data = [
+        "config" => $configs,
+        "type" => $type,
+    ];
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    return $result;
+}
