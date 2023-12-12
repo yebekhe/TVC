8f0e8434296dfe578e1306f4b3cc94f3f82ca02b
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-12-13 00:10:02 +0330

    Update sort.php

--- sort.php
+++ sort.php
@@ -1,48 +1,78 @@
 <?php
+// Enable error reporting
 ini_set("display_errors", 1);
 ini_set("display_startup_errors", 1);
 error_reporting(E_ERROR | E_PARSE);
 
+// Include the functions file
 require "functions.php";
 
+// Define the hash for each config type
+$configsHash = [
+    "vmess" => "ps",
+    "vless" => "hash",
+    "trojan" => "hash",
+    "tuic" => "hash",
+    "hy2" => "hash",
+    "ss" => "name",
+];
+
+// Read the config file and split it into an array by newline
 $configsArray = explode("\n", file_get_contents("config.txt"));
 
-$sortArray = [];
+// Initialize arrays to store deduplicated configs and their names
+$deduplicateArray = [];
+$namesArray = [];
 
+// Loop through each config in the configsArray
 foreach ($configsArray as $config) {
+    // Detect the type of the config
     $configType = detect_type($config);
-    $sortArray[$configType][] = $config;
-    if ($configType === "vless" && is_reality($config)) {
-        $sortArray["reality"][] = $config;
+    // Get the hash for the config type
+    $configHash = $configsHash[$configType];
+    // Parse the config
+    $decodedConfig = configParse($config);
+    // Store the hash temporarily
+    $tempHash = $decodedConfig[$configHash];
+    // Remove the hash from the config
+    unset($decodedConfig[$configHash]);
+    // Reparse the config without the hash
+    $encodedConfig = reparseConfig($decodedConfig, $configType);
+    // If the config is not already in the deduplicateArray, add it
+    if (!in_array($encodedConfig, $deduplicateArray)) {
+        $namesArray[] = $tempHash;
+        $deduplicateArray[] = $encodedConfig;
     }
 }
 
-foreach ($sortArray as $type => $sort) {
-    if ($type !== "") {
-        $tempConfigs = implode("\n", $sort);
-        $base64TempConfigs = base64_encode($tempConfigs);
-        file_put_contents("subscriptions/xray/normal/" . $type, $tempConfigs);
-        file_put_contents(
-            "subscriptions/xray/base64/" . $type,
-            $base64TempConfigs
-        );
-        file_put_contents(
-            "subscriptions/singbox/" . $type . ".json",
-            toSingbox($base64TempConfigs)
-        );
-        file_put_contents(
-            "subscriptions/clash/" . $type . ".yaml",
-            toClash($base64TempConfigs, "clash")
-        );
-        file_put_contents(
-            "subscriptions/meta/" . $type . ".yaml",
-            toClash($base64TempConfigs, "meta")
-        );
-        file_put_contents(
-            "subscriptions/surfboard/" . $type,
-            toClash($base64TempConfigs, "surfboard")
-        );
-    }
+// Initialize an array to store the final output
+$finalOutput = [];
+
+// Loop through each deduplicated config
+foreach ($deduplicateArray as $key => $deduplicate) {
+    // Detect the type of the config
+    $configType = detect_type($deduplicate);
+    // Get the hash for the config type
+    $configHash = $configsHash[$configType];
+    // Parse the config
+    $decodedConfig = configParse($deduplicate);
+    // Replace the hash with the name
+    $decodedConfig[$configHash] = $namesArray[$key];
+    // Reparse the config with the name
+    $encodedConfig = reparseConfig($decodedConfig, $configType);
+    // Add the config to the final output
+    $finalOutput[] = $encodedConfig;
 }
 
-echo "done!";
+// Write the final output to the config file
+file_put_contents("config.txt", implode("\n", $finalOutput));
+// Write the final output to the subscriptions/xray/normal/mix file
+file_put_contents("subscriptions/xray/normal/mix", implode("\n", $finalOutput));
+// Write the final output to the subscriptions/xray/base64/mix file, encoded in base64
+file_put_contents(
+    "subscriptions/xray/base64/mix",
+    implode("\n", base64_encode($finalOutput))
+);
+
+// Print "done!" to the console
+echo "Sorting Done!\n";
