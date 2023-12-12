d96ca6cc69c48e4c8ad86782b45ddcdc7aae296f
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-12-12 21:34:30 +0330

    Update sort.php

--- sort.php
+++ sort.php
@@ -33,7 +33,50 @@ function is_reality($input)
     return false;
 }
 
-$configsArray = explode("\n", file_get_contents("config.txt"));
+function toSingbox(
+    $configs
+) {
+    $url = "https://api.yebekhe.link/convertor/singbox/index.php";
+
+    $data = [
+        "config" => $configs,
+        "limit" => 0,
+        "tun" => true
+    ];
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    return $result;
+}
+
+function toClash(
+    $configs,
+    $type
+) {
+    $url = "https://api.yebekhe.link/convertor/clash/index.php";
+
+    $data = [
+        "config" => $configs,
+        "type" => $type,
+    ];
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    return $result;
+}
+
+
+$configsArray = explode("\n", file_get_contents("subscription/normal/mix"));
 
 $sortArray = [];
 
@@ -47,7 +90,13 @@ foreach($configsArray as $config) {
 
 foreach ($sortArray as $type => $sort) {
     if ($type !== "") {
-        file_put_contents($type . ".txt", implode("\n", $sort));
+        $tempConfigs = implode("\n", $sort);
+        file_put_contents("subscription/xray/normal/" . $type, $tempConfigs);
+        file_put_contents("subscription/xray/base64/" . $type, base64_encode($tempConfigs));
+        file_put_contents("subscription/singbox/" . $type . ".json", toSingbox($tempConfigs));
+        file_put_contents("subscription/clash/" . $type . ".yaml", toClash($tempConfigs, "clash"));
+        file_put_contents("subscription/meta/" . $type . ".yaml", toClash($tempConfigs, "meta"));
+        file_put_contents("subscription/surfboard/" . $type, toClash($tempConfigs, "surfboard"));
     }
 }
 
