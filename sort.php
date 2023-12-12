57906235f2a30c1bde898cdb3d5aa29048e4436d
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-12-13 00:27:43 +0330

    Update sort.php

--- sort.php
+++ sort.php
@@ -7,72 +7,58 @@ error_reporting(E_ERROR | E_PARSE);
 // Include the functions file
 require "functions.php";
 
-// Define the hash for each config type
-$configsHash = [
-    "vmess" => "ps",
-    "vless" => "hash",
-    "trojan" => "hash",
-    "tuic" => "hash",
-    "hy2" => "hash",
-    "ss" => "name",
-];
-
-// Read the config file and split it into an array by newline
+// Read the config.txt file and split it into an array by newline
 $configsArray = explode("\n", file_get_contents("config.txt"));
 
-// Initialize arrays to store deduplicated configs and their names
-$deduplicateArray = [];
-$namesArray = [];
+// Initialize an empty array to hold the sorted configurations
+$sortArray = [];
 
-// Loop through each config in the configsArray
+// Loop through each configuration in the configsArray
 foreach ($configsArray as $config) {
-    // Detect the type of the config
+    // Detect the type of the configuration
     $configType = detect_type($config);
-    // Get the hash for the config type
-    $configHash = $configsHash[$configType];
-    // Parse the config
-    $decodedConfig = configParse($config);
-    // Store the hash temporarily
-    $tempHash = $decodedConfig[$configHash];
-    // Remove the hash from the config
-    unset($decodedConfig[$configHash]);
-    // Reparse the config without the hash
-    $encodedConfig = reparseConfig($decodedConfig, $configType);
-    // If the config is not already in the deduplicateArray, add it
-    if (!in_array($encodedConfig, $deduplicateArray)) {
-        $namesArray[] = $tempHash;
-        $deduplicateArray[] = $encodedConfig;
+    // Add the configuration to the corresponding array in sortArray
+    $sortArray[$configType][] = $config;
+    // If the configuration is of type "vless" and is a reality, add it to the "reality" array
+    if ($configType === "vless" && is_reality($config)) {
+        $sortArray["reality"][] = $config;
     }
 }
 
-// Initialize an array to store the final output
-$finalOutput = [];
-
-// Loop through each deduplicated config
-foreach ($deduplicateArray as $key => $deduplicate) {
-    // Detect the type of the config
-    $configType = detect_type($deduplicate);
-    // Get the hash for the config type
-    $configHash = $configsHash[$configType];
-    // Parse the config
-    $decodedConfig = configParse($deduplicate);
-    // Replace the hash with the name
-    $decodedConfig[$configHash] = $namesArray[$key];
-    // Reparse the config with the name
-    $encodedConfig = reparseConfig($decodedConfig, $configType);
-    // Add the config to the final output
-    $finalOutput[] = $encodedConfig;
+// Loop through each type of configuration in sortArray
+foreach ($sortArray as $type => $sort) {
+    // If the type is not empty
+    if ($type !== "") {
+        // Join the configurations into a string, encode it to base64, and write it to a file
+        $tempConfigs = implode("\n", $sort);
+        $base64TempConfigs = base64_encode($tempConfigs);
+        file_put_contents("subscriptions/xray/normal/" . $type, $tempConfigs);
+        file_put_contents(
+            "subscriptions/xray/base64/" . $type,
+            $base64TempConfigs
+        );
+        // Convert the base64 encoded string to Singbox format and write it to a file
+        file_put_contents(
+            "subscriptions/singbox/" . $type . ".json",
+            toSingbox($base64TempConfigs)
+        );
+        // Convert the base64 encoded string to Clash format and write it to a file
+        file_put_contents(
+            "subscriptions/clash/" . $type . ".yaml",
+            toClash($base64TempConfigs, "clash")
+        );
+        // Convert the base64 encoded string to Meta format and write it to a file
+        file_put_contents(
+            "subscriptions/meta/" . $type . ".yaml",
+            toClash($base64TempConfigs, "meta")
+        );
+        // Convert the base64 encoded string to Surfboard format and write it to a file
+        file_put_contents(
+            "subscriptions/surfboard/" . $type,
+            toClash($base64TempConfigs, "surfboard")
+        );
+    }
 }
 
-// Write the final output to the config file
-file_put_contents("config.txt", implode("\n", $finalOutput));
-// Write the final output to the subscriptions/xray/normal/mix file
-file_put_contents("subscriptions/xray/normal/mix", implode("\n", $finalOutput));
-// Write the final output to the subscriptions/xray/base64/mix file, encoded in base64
-file_put_contents(
-    "subscriptions/xray/base64/mix",
-    implode("\n", base64_encode($finalOutput))
-);
-
 // Print "done!" to the console
-echo "Sorting Done!\n";
+echo "done!";
