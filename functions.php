ddede61ef20b4eb4919aac6ffec6903e8c96e60f
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-12-14 23:49:32 +0330

    Update functions.php

--- functions.php
+++ functions.php
@@ -278,27 +278,6 @@ function isBase64($input)
     return false;
 }
 
-function toSingbox(
-    $configs
-) {
-    $url = "https://api.yebekhe.link/convertor/singbox/index.php";
-
-    $data = [
-        "config" => $configs,
-        "limit" => 0,
-        "tun" => true
-    ];
-
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-    $result = curl_exec($ch);
-    curl_close($ch);
-
-    return $result;
-}
-
 function toClash(
     $configs,
     $type
