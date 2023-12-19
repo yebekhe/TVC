3fc094464ef3183bdaf88379d68e13c9fd93874e
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-12-19 22:51:41 +0330

    Update functions.php

--- functions.php
+++ functions.php
@@ -309,3 +309,13 @@ function tehran_time()
 
     return $formatted_time;
 }
+
+function hiddifyHeader ($subscriptionName) {
+    return "#profile-title: base64:" . base64_encode($subscriptionName) . "
+    #profile-update-interval: 1
+    #subscription-userinfo: upload=0; download=0; total=10737418240000000; expire=2546249531
+    #support-url: https://t.me/v2raycollector
+    #profile-web-page-url: https://github.com/yebekhe/TelegramV2rayCollector
+    
+    ";
+}
