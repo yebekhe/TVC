966584afdb78c85bb3c29a1e4c802c21ac5fcc87
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2024-03-10 01:52:44 +0330

    Update hiddifyWarp.php

--- hiddifyWarp.php
+++ hiddifyWarp.php
@@ -4,39 +4,22 @@ ini_set("display_errors", 1);
 ini_set("display_startup_errors", 1);
 error_reporting(E_ERROR | E_PARSE);
 
-function getRandomIpFromCidr($cidr) {
-    // Split the CIDR into network and mask
-    list($network, $mask) = explode('/', $cidr);
-
-    // Convert the network to binary
-    $networkBinary = ip2long($network);
-
-    // Calculate the number of available IPs
-    $availableIps = pow(2, (32 - $mask));
-
-    // Generate a random number of available IPs
-    $randomNumber = mt_rand(0, $availableIps - 1);
-
-    // Convert the random number to binary
-    $randomNumberBinary = str_pad(decbin($randomNumber), 32, '0', STR_PAD_LEFT);
-
-    // Combine the network and random number binaries
-    $ipBinary = $networkBinary . $randomNumberBinary;
-
-    // Convert the binary back to an IP address
-    $ip = long2ip($ipBinary);
-
-    return $ip;
+function getRandomIp($ip) {
+    $randomNumber = rand(0,255);
+    $ipArray = explode(".", $ip);
+    unset($ipArray[3]);
+    $newIp = implode(".", $ipArray) . "." . $randomNumber;
+    return $newIp;
 }
 
 $ipRanges = [
-    "162.159.192.0/24",
-    "162.159.193.0/24",
-    "162.159.195.0/24",
-    "188.114.96.0/24",
-    "188.114.97.0/24",
-    "188.114.98.0/24",
-    "188.114.99.0/24"
+    "162.159.192.0",
+    "162.159.193.0",
+    "162.159.195.0",
+    "188.114.96.0",
+    "188.114.97.0",
+    "188.114.98.0",
+    "188.114.99.0"
 ];
 
 $ports = "500,854,859,864,878,880,890,891,894,903,908,928,934,939,942,943, 945,946,955,968,987,988,1002,1010,1014,1018,1070,1074,1180, 1387,1701,1843,2371,2408,2506,3138,3476,3581, 3854,4177,4198,4233,4500,5279,5956,7103,7152, 7156,7281,7559,8319,8742,8854,8886";
@@ -47,8 +30,8 @@ $choosenRanges = [
     $ipRanges[array_rand($ipRanges)]
 ];
 $choosenIps = [
-    getRandomIpFromCidr($choosenRange[0]),
-    getRandomIpFromCidr($choosenRange[1])
+    getRandomIp($choosenRange[0]),
+    getRandomIp($choosenRange[1])
 ];
 $portsArray = explode(",", $ports);
 $choosenPort = $portsArray[array_rand($portsArray)];
